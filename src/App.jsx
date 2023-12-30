dde5b9838eec21096743fd626826461220a068a8
outdir <outdir@outlook.com>
2023-12-30 12:15:00 +0330

    V FD 2-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import FooterComponent from "./components/ui/Footer.component";
 import NavabarComponent from "./components/ui/Navabar.component";
- 
+
+import { Routes, Route } from 'react-router-dom';
+import HomePage from "./pages/Home.page";
+import AboutUsPage from "./pages/AboutUs.page";
+import ContactUsPage from "./pages/ContactUs.page";
+import FAQPage from "./pages/FAQ.page";
+
 function App() {
   return (
     <div>
-      {/* Navbar */}
+      {/* Navbar */} 
       <NavabarComponent />
       {/* Wrapper */}
       <div className="wrapper">
-        <h2>wrapper</h2>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/faq" element={<FAQPage />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
+          <Route path="/contact-us" element={<ContactUsPage />} />
+        </Routes>
       </div>
       {/* Footer */}
       <FooterComponent />
@@ -17,3 +28,10 @@ function App() {
 }
 
 export default App;
+
+/*
+/
+/faq
+/about-us
+/contact-us
+*/
