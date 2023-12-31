ffe56d9eb14ad9cad07cafa6adcfcfc6cee3faf6
outdir <outdir@outlook.com>
2023-12-31 10:26:24 +0330

    V FD 2-8 : contact-us page

--- src/pages/ContactUs.page.jsx
+++ src/pages/ContactUs.page.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import contactImg from '../assets/images/contact-us.jpg'
 
 const ContactUsPage = () => {
     return (
-        <div>
-            <h1>ContactUs</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-12 pt-3 rounded">
+                    <h3>Contact us</h3>
+                </div>
+            </div>
+            <div className="row align-items-center">
+                <div className="col-md-6 pt-3">
+                    <div className="mb-3">
+                        <label className="form-label">Your Name:</label>
+                        <input type="text" className="form-control" placeholder="Please type your name." />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Your Message:</label>
+                        <input type="text" className="form-control" placeholder="Please type your message." />
+                    </div>
+                    <button className="btn btn-outline-primary ml-2">Send</button>
+                </div>
+                <div className="col-md-6 text-center pt-3">
+                    <img className="img-fluid" src={contactImg} alt="contact-us" />
+                </div>
+            </div>
         </div>
     );
 };
