2a9a37d9eb1af5cc5e2d0b5424a58a081108a08d
outdir <outdir@outlook.com>
2023-12-31 11:54:50 +0330

    V FD 3-1 (1/2) : api response

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SpinnerComponent from "../components/ui/Spinner.component";
+import axios from "axios";
+import { baseUrl } from "../constants/api";
 
 const HomePage = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        setLoading(true);
+        axios(baseUrl)
+            .then(response => {
+                setLoading(false);
+                console.log(response.data);
+            })
+            .catch(error => {
+                setError("Error occured!");
+            })
+    }, []);
+
     return (
-        <div>
-            <h1>HomePage</h1>
-            <SpinnerComponent />
+        <div className="container">
+            <div className="row">
+                <div className="col-12 text-center">
+                    <h1>CryptoRadar</h1>
+                    <h4>A place to manage all of your trade informations and actions</h4>
+                    <h4>Do not forget to always using asset and risk managemant!</h4>
+                </div>
+            </div>
         </div>
     );
 };
